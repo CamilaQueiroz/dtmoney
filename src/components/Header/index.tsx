220c224ada5383588e1f248af63522011a8c417c
Camila Queiroz <camilaqueiroz@mbp-de-camila.home>
2021-04-28 14:30:54 -0300

    Tirando o modal do header e passando a funcao como props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransacionModal: () => void;
+}
+
+export function Header({ onOpenNewTransacionModal }: HeaderProps) {
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money"/>
 
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransacionModal}>
           Nova transação
         </button>
       </Content>
